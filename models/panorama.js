5ac30d1205c91a79bbefb97b07298d53768194a8
Wose <Jacksonteather@hotmail.com>
2016-03-28 12:27:58 -0400

    Panorama model
    Added GET and POST routes for a basic pano config model with no hotspot references

--- models/panorama.js
+++ models/panorama.js
@@ -4,53 +4,27 @@ var Schema = mongoose.Schema;
 // See http://mongoosejs.com/docs/schematypes.html
 
 var PanoramaSchema = new Schema({
-	//name: String,
-	type: {type: String, required: true, default: "panoramas"}, // this version requires this field to exist"type": "panoramas",
-  id: String,
-  attributes: {
-    title: {type: String, required: true},
-    hfov: Number,
-    pitch: Number,
-    yaw: Number,
-    northOffset: Number,
-    type: {type: String, default: "multires"},
-    multiRes: {
-      basePath: String,
-        path: String,
-        fallbackPath: String,
-        extension: String,
-        tileResolution: Number,
-        maxLevel: Number,
-        cubeResolution: Number
-    }
-  },
-  links: {
-    self: function() {
-    	return "v1/"+this.type+"/"+this.id;
-    }
-  },
-  "relationships": {
-    "pois": {
-      "links": {
-        "self": "v1/pois/pc",
-        "related": "v1/panoramas/pc/pois"
-      },
-      "data": { "type": "pois", "id": "pc" }
-    },
-    "hotspots": {
-      "links": {
-        "self": "v1/hotspots/pc",
-        "related": "v1/pois/pc/hotspots"
-      },
-      "data": [
-        { "type": "hotspots", "id": "5" },
-        { "type": "hotspots", "id": "12" },
-        { "type": "hotspots", "id": "22" },
-        { "type": "hotspots", "id": "24" }
-      ]
-    }
+	code: {type: String, required: true, unique: true, index: true },
+  title: {type: String, required: true},
+  hfov: { type: Number, default: 100 },
+  pitch: { type: Number, default: 10 },
+  yaw: { type: Number, default: 50 },
+  northOffset: { type: Number, default: 0 },
+  type: { type: String, default: "multires" },
+  multiRes: {
+    basePath: String,
+      path: { type: String, default: "/%l/%s%y_%x" },
+      fallbackPath: { type: String, default: "/fallback/%s" },
+      extension: { type: String, default: "jpg" },
+      tileResolution: Number,
+      maxLevel: Number,
+      cubeResolution: Number
   }
-})
+});
+
+PanoramaSchema.statics.findByCode = function (code, cb) {
+  return this.find({ code: code }, cb);
+}
 
 // export 'Animal' model so we can interact with it in other files
 module.exports = mongoose.model('Panorama',PanoramaSchema);
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var mongoose = require('mongoose');
 
 // our db model
-var Animal = require("../models/model.js");
+var Panorama = require("../models/panorama.js");
 
 /**
  * GET '/'
@@ -15,8 +15,8 @@ router.get('/', function(req, res) {
   
   var jsonData = {
   	'name': 'virtualtour-server',
-    'version': '1.0',
-  	'status':'200'
+    'version': 1.0,
+  	'status': 200
   }
 
   // respond with json data
@@ -29,56 +29,68 @@ router.get('/docs', function(req,res){
 })
 
 // /**
-//  * POST '/api/create'
-//  * Receives a POST request of the new user and location, saves to db, responds back
-//  * @param  {Object} req. An object containing the different attributes of the Person
+//  * POST '/panoramas'
+//  * Receives a POST request of the new panorama config, saves to db, responds back
+//  * @param  {Object} req. An object containing the different attributes of the Panorama
 //  * @return {Object} JSON
 //  */
 
-router.post('/api/create', function(req, res){
+router.post('/panoramas', function(req, res){
 
     console.log(req.body);
 
     // pull out the information from the req.body
-    var name = req.body.name;
-    var age = req.body.age;
-    var tags = req.body.tags.split(","); // split string into array
-    var weight = req.body.weight;
-    var color = req.body.color;
-    var url = req.body.url;
+    var code = req.body.code;
+    var title = req.body.title;
+    var hfov = req.body.hfov;
+    var pitch = req.body.pitch;
+    var yaw = req.body.yaw;
+    var northOffset = req.body.northOffset;
+    var type = req.body.type;
+    var basePath = req.body.basePath || "./assets/img/"+req.body.code;
+    var extension = req.body.extension;
+    var tileResolution = req.body.tileResolution;
+    var maxLevel = req.body.maxLevel;
+    var cubeResolution = req.body.cubeResolution;
 
     // hold all this data in an object
     // this object should be structured the same way as your db model
-    var animalObj = {
-      name: name,
-      age: age,
-      tags: tags,
-      description: {
-        weight: weight,
-        color: color
-      },
-      url: url
+    var panoObject = {
+      code: code,
+      title: title,
+      hfov: hfov,
+      pitch: pitch,
+      yaw: yaw,
+      northOffset: northOffset,
+      type: type,
+      multiRes: {
+        basePath: basePath,
+        extension: extension,
+        tileResolution: tileResolution,
+        maxLevel: maxLevel,
+        cubeResolution: cubeResolution
+      }
     };
 
     // create a new animal model instance, passing in the object
-    var animal = new Animal(animalObj);
+    var panorama = new Panorama(panoObject);
 
     // now, save that animal instance to the database
     // mongoose method, see http://mongoosejs.com/docs/api.html#model_Model-save    
-    animal.save(function(err,data){
+    panorama.save(function(err,data){
       // if err saving, respond back with error
       if (err){
-        var error = {status:'ERROR', message: 'Error saving animal'};
+        var error = {status:400, message: 'Error saving panorama', error: err};
         return res.json(error);
       }
 
-      console.log('saved a new animal!');
+      console.log('Saved a new panorama!');
       console.log(data);
 
       // now return the json data of the new animal
       var jsonData = {
-        status: 'OK',
-        animal: data
+        status: 200,
+        data: data
       }
 
       return res.json(jsonData);
@@ -87,29 +99,33 @@ router.post('/api/create', function(req, res){
 });
 
 // /**
-//  * GET '/api/get/:id'
+//  * GET '/panoramas/:id'
 //  * Receives a GET request specifying the animal to get
 //  * @param  {String} req.param('id'). The animalId
 //  * @return {Object} JSON
 //  */
 
-router.get('/api/get/:id', function(req, res){
+router.get('/panoramas/:id', function(req, res){
 
   var requestedId = req.param('id');
 
   // mongoose method, see http://mongoosejs.com/docs/api.html#model_Model.findById
-  Animal.findById(requestedId, function(err,data){
+  Panorama.findByCode(requestedId, function(err,data){
 
-    // if err or no user found, respond with error 
-    if(err || data == null){
-      var error = {status:'ERROR', message: 'Could not find that animal'};
+    // if err or no pano found, respond with error 
+    if (err) {
+      var error = {status: 400, message: 'Error retrieving panorama', error: err};
+       return res.json(error);
+    }
+    if (data == null) {
+      var error = {status: 404, message: 'Panorama not found'};
        return res.json(error);
     }
 
     // otherwise respond with JSON data of the animal
     var jsonData = {
-      status: 'OK',
-      animal: data
+      status: 200,
+      data: data
     }
 
     return res.json(jsonData);
@@ -123,21 +139,26 @@ router.get('/api/get/:id', function(req, res){
 //  * @return {Object} JSON
 //  */
 
-router.get('/api/get', function(req, res){
+router.get('/panoramas', function(req, res){
 
   // mongoose method to find all, see http://mongoosejs.com/docs/api.html#model_Model.find
-  Animal.find(function(err, data){
-    // if err or no animals found, respond with error 
-    if(err || data == null){
-      var error = {status:'ERROR', message: 'Could not find animals'};
-      return res.json(error);
+  Panorama.find(function(err, data){
+
+    // if err or no panos found, respond with error 
+    if (err) {
+      var error = {status: 400, message: 'Error retrieving panoramas', error: err};
+       return res.json(error);
+    }
+    if (data == null) {
+      var error = {status: 404, message: 'No panoramas found'};
+       return res.json(error);
     }
 
     // otherwise, respond with the data 
 
     var jsonData = {
-      status: 'OK',
-      animals: data
+      status: 200,
+      data: data
     } 
 
     res.json(jsonData);
@@ -154,78 +175,78 @@ router.get('/api/get', function(req, res){
 //  * @return {Object} JSON
 //  */
 
-router.post('/api/update/:id', function(req, res){
+// router.post('/api/update/:id', function(req, res){
 
-   var requestedId = req.param('id');
+//    var requestedId = req.param('id');
 
-   var dataToUpdate = {}; // a blank object of data to update
+//    var dataToUpdate = {}; // a blank object of data to update
 
-    // pull out the information from the req.body and add it to the object to update
-    var name, age, weight, color, url; 
+//     // pull out the information from the req.body and add it to the object to update
+//     var name, age, weight, color, url; 
 
-    // we only want to update any field if it actually is contained within the req.body
-    // otherwise, leave it alone.
-    if(req.body.name) {
-      name = req.body.name;
-      // add to object that holds updated data
-      dataToUpdate['name'] = name;
-    }
-    if(req.body.age) {
-      age = req.body.age;
-      // add to object that holds updated data
-      dataToUpdate['age'] = age;
-    }
-    if(req.body.weight) {
-      weight = req.body.weight;
-      // add to object that holds updated data
-      dataToUpdate['description'] = {};
-      dataToUpdate['description']['weight'] = weight;
-    }
-    if(req.body.color) {
-      color = req.body.color;
-      // add to object that holds updated data
-      if(!dataToUpdate['description']) dataToUpdate['description'] = {};
-      dataToUpdate['description']['color'] = color;
-    }
-    if(req.body.url) {
-      url = req.body.url;
-      // add to object that holds updated data
-      dataToUpdate['url'] = url;
-    }
+//     // we only want to update any field if it actually is contained within the req.body
+//     // otherwise, leave it alone.
+//     if(req.body.name) {
+//       name = req.body.name;
+//       // add to object that holds updated data
+//       dataToUpdate['name'] = name;
+//     }
+//     if(req.body.age) {
+//       age = req.body.age;
+//       // add to object that holds updated data
+//       dataToUpdate['age'] = age;
+//     }
+//     if(req.body.weight) {
+//       weight = req.body.weight;
+//       // add to object that holds updated data
+//       dataToUpdate['description'] = {};
+//       dataToUpdate['description']['weight'] = weight;
+//     }
+//     if(req.body.color) {
+//       color = req.body.color;
+//       // add to object that holds updated data
+//       if(!dataToUpdate['description']) dataToUpdate['description'] = {};
+//       dataToUpdate['description']['color'] = color;
+//     }
+//     if(req.body.url) {
+//       url = req.body.url;
+//       // add to object that holds updated data
+//       dataToUpdate['url'] = url;
+//     }
 
-    var tags = []; // blank array to hold tags
-    if(req.body.tags){
-      tags = req.body.tags.split(","); // split string into array
-      // add to object that holds updated data
-      dataToUpdate['tags'] = tags;
-    }
+//     var tags = []; // blank array to hold tags
+//     if(req.body.tags){
+//       tags = req.body.tags.split(","); // split string into array
+//       // add to object that holds updated data
+//       dataToUpdate['tags'] = tags;
+//     }
 
 
-    console.log('the data to update is ' + JSON.stringify(dataToUpdate));
+//     console.log('the data to update is ' + JSON.stringify(dataToUpdate));
 
-    // now, update that animal
-    // mongoose method findByIdAndUpdate, see http://mongoosejs.com/docs/api.html#model_Model.findByIdAndUpdate  
-    Animal.findByIdAndUpdate(requestedId, dataToUpdate, function(err,data){
-      // if err saving, respond back with error
-      if (err){
-        var error = {status:'ERROR', message: 'Error updating animal'};
-        return res.json(error);
-      }
+//     // now, update that animal
+//     // mongoose method findByIdAndUpdate, see http://mongoosejs.com/docs/api.html#model_Model.findByIdAndUpdate  
+//     Animal.findByIdAndUpdate(requestedId, dataToUpdate, function(err,data){
+//       // if err saving, respond back with error
+//       if (err){
+//         var error = {status:'ERROR', message: 'Error updating animal'};
+//         return res.json(error);
+//       }
 
-      console.log('updated the animal!');
-      console.log(data);
+//       console.log('updated the animal!');
+//       console.log(data);
 
-      // now return the json data of the new person
-      var jsonData = {
-        status: 'OK',
-        animal: data
-      }
+//       // now return the json data of the new person
+//       var jsonData = {
+//         status: 'OK',
+//         animal: data
+//       }
 
-      return res.json(jsonData);
+//       return res.json(jsonData);
 
-    })
+//     })
 
-})
+// })
 
 /**
  * GET '/api/delete/:id'
@@ -234,27 +255,27 @@ router.post('/api/update/:id', function(req, res){
  * @return {Object} JSON
  */
 
-router.get('/api/delete/:id', function(req, res){
+// router.get('/api/delete/:id', function(req, res){
 
-  var requestedId = req.param('id');
+//   var requestedId = req.param('id');
 
-  // Mongoose method to remove, http://mongoosejs.com/docs/api.html#model_Model.findByIdAndRemove
-  Animal.findByIdAndRemove(requestedId,function(err, data){
-    if(err || data == null){
-      var error = {status:'ERROR', message: 'Could not find that animal to delete'};
-      return res.json(error);
-    }
+//   // Mongoose method to remove, http://mongoosejs.com/docs/api.html#model_Model.findByIdAndRemove
+//   Animal.findByIdAndRemove(requestedId,function(err, data){
+//     if(err || data == null){
+//       var error = {status:'ERROR', message: 'Could not find that animal to delete'};
+//       return res.json(error);
+//     }
 
-    // otherwise, respond back with success
-    var jsonData = {
-      status: 'OK',
-      message: 'Successfully deleted id ' + requestedId
-    }
+//     // otherwise, respond back with success
+//     var jsonData = {
+//       status: 'OK',
+//       message: 'Successfully deleted id ' + requestedId
+//     }
 
-    res.json(jsonData);
+//     res.json(jsonData);
 
-  })
+//   })
 
-})
+// })
 
 module.exports = router;
